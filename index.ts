4f2fc4291f6e7682f6db68c417b624438f768469
mgechev <mgechev@gmail.com>
2017-03-11 15:24:26 -0800

    feat: export provider symbols

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export {ContextSymbols} from './lib/context-symbols';
 export {ProjectSymbols, ProgramFactory, LazyModuleResolver} from './lib/project-symbols';
 export {ModuleSymbol} from './lib/module-symbol';
+export {ProviderSymbol} from './lib/provider-symbol';
 export {DirectiveSymbol} from './lib/directive-symbol';
 export {PipeSymbol} from './lib/pipe-symbol';
 export {Symbol} from './lib/symbol';
