a181daf7bdeb9a19a4e03b39d708d355d26f2c55
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-06-19 22:07:37 +0200

    rename workspace-symbols

--- lib/ngtsc/workspace-symbols.ts
+++ lib/ngtsc/workspace-symbols.ts
@@ -1,65 +1,99 @@
-import { NgCompilerHost, NgCompiler } from '@angular/compiler-cli/src/ngtsc/core';
+import { createProgram, Program, createModuleResolutionCache, TypeChecker, getOriginalNode, Declaration } from 'typescript';
+import { readConfiguration } from '@angular/compiler-cli';
+import { NgCompilerHost } from '@angular/compiler-cli/src/ngtsc/core';
 import { NgCompilerOptions } from '@angular/compiler-cli/src/ngtsc/core/api';
 import { InjectableDecoratorHandler, PipeDecoratorHandler, DirectiveDecoratorHandler, ReferencesRegistry, NoopReferencesRegistry, NgModuleDecoratorHandler, ComponentDecoratorHandler } from '@angular/compiler-cli/src/ngtsc/annotations';
-import { NgtscCompilerHost, FileSystem, LogicalFileSystem } from '@angular/compiler-cli/src/ngtsc/file_system';
-import { createProgram, Program, createModuleResolutionCache } from 'typescript';
+import { NgtscCompilerHost, FileSystem, LogicalFileSystem, NodeJSFileSystem } from '@angular/compiler-cli/src/ngtsc/file_system';
 import { TypeScriptReflectionHost } from '@angular/compiler-cli/src/ngtsc/reflection';
 import { PartialEvaluator } from '@angular/compiler-cli/src/ngtsc/partial_evaluator';
 import { IncrementalDriver } from '@angular/compiler-cli/src/ngtsc/incremental';
-import { DefaultImportTracker, ReferenceEmitStrategy, AliasingHost, ReferenceEmitter, LogicalProjectStrategy, RelativePathStrategy, PrivateExportAliasingHost, LocalIdentifierStrategy, AbsoluteModuleStrategy, AliasStrategy, UnifiedModulesStrategy, UnifiedModulesAliasingHost, ModuleResolver } from '@angular/compiler-cli/src/ngtsc/imports';
-import { InjectableClassRegistry, CompoundMetadataRegistry, DtsMetadataReader, LocalMetadataRegistry, MetadataReader, CompoundMetadataReader } from '@angular/compiler-cli/src/ngtsc/metadata';
+import { DefaultImportTracker, ReferenceEmitStrategy, AliasingHost, Reference, ReferenceEmitter, LogicalProjectStrategy, RelativePathStrategy, PrivateExportAliasingHost, LocalIdentifierStrategy, AbsoluteModuleStrategy, AliasStrategy, UnifiedModulesStrategy, UnifiedModulesAliasingHost, ModuleResolver } from '@angular/compiler-cli/src/ngtsc/imports';
+import { InjectableClassRegistry, CompoundMetadataRegistry, DtsMetadataReader, LocalMetadataRegistry, CompoundMetadataReader } from '@angular/compiler-cli/src/ngtsc/metadata';
 import { MetadataDtsModuleScopeResolver, LocalModuleScopeRegistry, ComponentScopeReader } from '@angular/compiler-cli/src/ngtsc/scope';
-import { getSourceFileOrNull } from '@angular/compiler-cli/src/ngtsc/util/src/typescript';
+import { getSourceFileOrNull, isDtsPath } from '@angular/compiler-cli/src/ngtsc/util/src/typescript';
 import { NgModuleRouteAnalyzer } from '@angular/compiler-cli/src/ngtsc/routing';
 import { CycleAnalyzer, ImportGraph } from '@angular/compiler-cli/src/ngtsc/cycles';
+import { HostResourceLoader } from '@angular/compiler-cli/src/ngtsc/resource';
+import { ReferenceGraph } from '@angular/compiler-cli/src/ngtsc/entry_point';
 
 interface Toolkit {
   program: Program;
   host: NgCompilerHost;
+  // Handler
   injectableHandler: InjectableDecoratorHandler;
   pipeHandler: PipeDecoratorHandler;
   directiveHandler: DirectiveDecoratorHandler;
   moduleHandler: NgModuleDecoratorHandler;
   cmptHandler: ComponentDecoratorHandler;
-  checker;
-  reflector;
-  defaultImportTracker;
-  injectableRegistry;
-  // Directive + pipe
-  evaluator;
+
+  checker: TypeChecker;
+  reflector: TypeScriptReflectionHost;
+  defaultImportTracker: DefaultImportTracker;
+  injectableRegistry: InjectableClassRegistry;
+  evaluator: PartialEvaluator;
   dtsReader: DtsMetadataReader;
-  metaRegistry; // Directive + pipe + ngModule
-  scopeRegistry;// Directive + pipe + ngModule
-  metaReader;   // Directive + ngModule
+  metaRegistry: CompoundMetadataRegistry;
+  scopeReader: ComponentScopeReader;
+  metaReader: CompoundMetadataReader;
   aliasingHost: AliasingHost;
   localMetaReader: LocalMetadataRegistry;
+  refEmitter: ReferenceEmitter;
+  referencesRegistry: ReferencesRegistry;
+  routeAnalyzer: NgModuleRouteAnalyzer;
 
-  // Module + Component
-  scopeReader: LocalModuleScopeRegistry;  // Component
-  refEmitter;   // Component + ngModule
-  referencesRegistry; // ngModule
-  routeAnalyzer; // ngModule
-
-  // from Constructor
-  resourceManager;  // Component
-  moduleResolver;   // Component
-  cycleAnalyzer;    // Component
-  incrementalDriver;// Component Only depgraph is needed
+  resourceManager: HostResourceLoader;
+  moduleResolver: ModuleResolver;
+  cycleAnalyzer: CycleAnalyzer;
+  incrementalDriver: IncrementalDriver;
 }
 
-class ProjectSymbol {
+type ToolkitRecord = Partial<{
+  [key in keyof Toolkit]: Toolkit[key]
+}>;
+
+// code from :
+// https://github.com/angular/angular/blob/9.1.x/packages/compiler-cli/src/ngtsc/core/src/compiler.ts#L821
+class ReferenceGraphAdapter implements ReferencesRegistry {
+  constructor(private graph: ReferenceGraph) {}
+
+  add(source: Declaration, ...references: Reference<Declaration>[]): void {
+    for (const {node} of references) {
+      let sourceFile = node.getSourceFile();
+      if (sourceFile === undefined) {
+        sourceFile = getOriginalNode(node).getSourceFile();
+      }
+
+      // Only record local references (not references into .d.ts files).
+      if (sourceFile === undefined || !isDtsPath(sourceFile.fileName)) {
+        this.graph.add(source, node);
+      }
+    }
+  }
+}
+
+
+
+export class WorkspaceSymbols {
   private options: NgCompilerOptions;
-  private toolkit: Partial<Record<keyof Toolkit, any>> = {};
+  private rootNames: string[];
+  private toolkit: ToolkitRecord = {};
   private isCore = false;
 
-  constructor(private fs: FileSystem) {}
+  constructor(
+    private tsconfigPath: string,
+    private fs: FileSystem = new NodeJSFileSystem(),
+  ) {
+    const config = readConfiguration(this.tsconfigPath);
+    this.options = config.options;
+    this.rootNames = config.rootNames;
+  }
 
 
   /** Angular wrapper around the typescript host compiler */
   get host() {
     return this.lazy('host', () => {
       const baseHost = new NgtscCompilerHost(this.fs, this.options);
-      return NgCompilerHost.wrap(baseHost, inputFiles, this.options);
+      return NgCompilerHost.wrap(baseHost, this.rootNames, this.options);
     });
   }
 
@@ -182,10 +216,8 @@ class ProjectSymbol {
 
   private get incrementalDriver() {
     return this.lazy('incrementalDriver', () => {
-      const incrementalStrategy = new TrackedIncrementalBuildStrategy(); // Given on the constructor
       // @todo() support oldProgram https://github.com/angular/angular/blob/master/packages/compiler-cli/src/ngtsc/core/src/compiler.ts#L130
       const incrementalDriver = IncrementalDriver.fresh(this.program);
-      incrementalStrategy.setIncrementalDriver(incrementalDriver, this.program);
       return incrementalDriver;
     });
   }
@@ -199,7 +231,7 @@ class ProjectSymbol {
   }
 
   private get resourceManager() {
-    return this.lazy('resourceManager', () => new AdapterResourceLoader(this.host, this.options));
+    return this.lazy('resourceManager', () => new HostResourceLoader(this.host, this.options));
   }
 
   private get moduleResolver() {
@@ -278,11 +310,11 @@ class ProjectSymbol {
 
   // alias of scopeReader
   private get scopeRegistry() {
-    return this.scopeReader;
+    return this.scopeReader as LocalModuleScopeRegistry;
   }
 
   private get metaRegistry() {
-    return this.lazy('metaRegistry', () => new CompoundMetadataRegistry([this.localMetaReader, this.scopeReader]));
+    return this.lazy('metaRegistry', () => new CompoundMetadataRegistry([ this.localMetaReader, this.scopeRegistry ]));
   }
 
   private get metaReader() {
@@ -314,10 +346,12 @@ class ProjectSymbol {
   }
 
 
-  private lazy<K extends keyof Toolkit>(key: K, load: () => Toolkit[K]): Toolkit[K] {
+  private lazy<K extends keyof ToolkitRecord>(key: K, load: () => ToolkitRecord[K]): ToolkitRecord[K] {
     if (!this.toolkit[key]) {
       this.toolkit[key] = load();
     }
     return this.toolkit[key];
   }
 }
+
+
