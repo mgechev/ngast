9d2216ab7347d490d006de5c874af83538df9478
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2020-08-30 18:13:00 +0200

    remove old code

--- lib/ngcc/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './symbol';
-export * from './directive-symbol';
-export * from './module-symbol';
-export * from './pipe-symbol';
-export * from './project-symbols';
-export * from './provider-symbol';
-export * from './resource-resolver';
--- test/ngcc/get-tsconfig.ts
+++
@@ -1,5 +0,0 @@
-import { join } from "path";
-
-export function getConfig(name: string) {
-  return join(__dirname, '/../../../test/fixture', name, 'tsconfig.json');
-}
